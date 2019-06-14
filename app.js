65a61426c9f7004ca5b1719290e5eac91a19d906
venkg01 <venkg01@gmail.com>
2019-06-14 19:20:11 -0400

    first commit

---
+++ app.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const morgan = require("morgan");
+const path = require("path");
+const server = express();
+const proxy = require("http-proxy-middleware");
+const cors = require('cors');
+
+const PORT = process.env.PORT || 3000;
+server.use(cors())
+server.use(morgan("dev"));
+server.use(express.static(path.join(__dirname, "public")));
+
+server.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, "/public/index.html"));
+});
+
+server.listen(PORT, () => {
+  console.log(`server running at: http://localhost:${PORT}`);
+});
